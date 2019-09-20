c665f7ffee40752166726190dd098bc2adf2700d
cougarharlowe@gmail.com <fowlfighter7297>
2019-09-20 13:59:25 -0700

    Need to work on the loading of sounds

--- app/src/main/java/com/example/sounds/SoundsActivity.java
+++ app/src/main/java/com/example/sounds/SoundsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.media.AudioManager;
@@ -16,6 +17,11 @@ public class SoundsActivity extends AppCompatActivity {
     private int aNote;
     private int bNote;
     private int cNote;
+    private int dNote;
+    private int eNote;
+    private int fNote;
+    private int gNote;
+    private float volume;
     private boolean isLoaded = false;
     private SoundPool SoundPool;
 
@@ -31,6 +37,19 @@ public class SoundsActivity extends AppCompatActivity {
     }
 
     private void loadSoundPool() {
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        SoundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        SoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(android.media.SoundPool soundPool, int i, int i1) {
+                isLoaded = true;
+
+            }
+        });
+
+        aNote = SoundPool.load(this, R.raw.scalea, 1);
+        bNote = SoundPool.load(this, R.raw.scaleb, 1);
+        cNote = SoundPool.load(this, R.raw.scalec,1);
 
     }
 
@@ -43,21 +62,55 @@ public class SoundsActivity extends AppCompatActivity {
         buttonSoundA.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                setVolumeControlStream(AudioManager.STREAM_MUSIC);
-                SoundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-                SoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-                    @Override
-                    public void onLoadComplete(android.media.SoundPool soundPool, int Anote) {
-                        isLoaded = true;
-                    }
-                });
-            aNote = SoundPool.load(buttonSoundA, R.raw.scalea, 1);
+                if (isLoaded) {
+                    AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                    float actualVolume = (float) audioManager
+                            .getStreamVolume(AudioManager.STREAM_MUSIC);
+                    float maxVolume = (float) audioManager
+                            .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                    float volume = actualVolume / maxVolume;
+                    SoundPool.play(aNote, volume, volume, 1, 0, 1f);
+                    Log.e("test", "Played sound");
+                     }
+
+        }
+
+
+
+        });
+        buttonSoundB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isLoaded) {
+                    AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                    float actualVolume = (float) audioManager
+                            .getStreamVolume(AudioManager.STREAM_MUSIC);
+                    float maxVolume = (float) audioManager
+                            .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                    float volume = actualVolume / maxVolume;
+                    SoundPool.play(bNote, volume, volume, 1, 0, 1f);
+                    Log.e("test", "Played sound");
+                }
+
+
             }
         });
+        buttonSoundC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isLoaded) {
+                    AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                    float actualVolume = (float) audioManager
+                            .getStreamVolume(AudioManager.STREAM_MUSIC);
+                    float maxVolume = (float) audioManager
+                            .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                    float volume = actualVolume / maxVolume;
+                    SoundPool.play(cNote, volume, volume, 1, 0, 1f);
+                    Log.e("test", "Played sound");
+                }
 
 
-
-    }
-
-
+            }
+        });
+}
 }
